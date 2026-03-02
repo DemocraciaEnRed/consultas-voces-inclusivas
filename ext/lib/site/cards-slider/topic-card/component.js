25041f8e8c8365c5842e644cbc180791a9727d73
xtian7489 <xtian7489@gmail.com>
2026-03-02 12:20:40 -0300

    mejorar estilos de tarjetas, agregar transición de color y cambiar a enlace en lugar de redirección

--- ext/lib/site/cards-slider/topic-card/component.js
+++ ext/lib/site/cards-slider/topic-card/component.js
@@ -6,42 +6,40 @@ import Social from 'lib/frontend/site/topic-layout/topic-article/social/componen
 import Timeago from 'lib/frontend/site/timeago'
 
 export default ({ topic }) => {
-  const linkTopic = () => { 
-    window.location.href = topic.url
-  }
+
   return (
-  <div 
-    onClick={linkTopic}
-    className='topic-card panel panel-default panel-md'>
-    <div
-      className='panel-heading'
-      style={{ backgroundImage: `url("${topicStore.getCoverUrl(topic)}")`}} />
-    <div className='topic-card-wrapper'>
-      { topic.closingAt &&
-        <p className='meta-information'>
-          <i className='icon-clock' />
-          <span className='time-ago-label'>
-            {(topic.closed ? t('common.closed') : t('common.close')) + ' '}
-          </span>
-          <Timeago className='meta-timeago' date={topic.closingAt} />
-        </p>
-      }
-      <div className='panel-body'>
-        <h3>{topic.mediaTitle}</h3>
-      
+    <Link
+      to={topic.url}
+      className='topic-card panel panel-default panel-md'>
+      <div
+        className='panel-heading'
+        style={{ backgroundImage: `url("${topicStore.getCoverUrl(topic)}")` }} />
+      <div className='topic-card-wrapper'>
+        {topic.closingAt &&
+          <p className='meta-information'>
+            <i className='icon-clock' />
+            <span className='time-ago-label'>
+              {(topic.closed ? t('common.closed') : t('common.close')) + ' '}
+            </span>
+            <Timeago className='meta-timeago' date={topic.closingAt} />
+          </p>
+        }
+        <div className='panel-body'>
+          <h3>{topic.mediaTitle}</h3>
+
+        </div>
+        <div className='topic-card-footer'>
+          <p className='text-muted'>
+            {topic.action.method === "" ?
+              `${topic.commentersCount} ${topic.commentersCount === 1 ? t('proposal-article.participant.singular') : t('proposal-article.participant.plural')}` :
+              `${topic.action.count} ${topic.action.count === 1 ? t('proposal-article.participant.singular') : t('proposal-article.participant.plural')}`
+            }
+          </p>
+          <div className='social-links'>
+            <Social topic={topic} />
+          </div>
+        </div>
       </div>
-      <div className='topic-card-footer'>
-        <p className='text-muted'>
-          {topic.action.method === "" ?
-          `${topic.commentersCount} ${topic.commentersCount === 1 ? t('proposal-article.participant.singular') : t('proposal-article.participant.plural')}` :
-          `${topic.action.count} ${topic.action.count === 1 ? t('proposal-article.participant.singular') : t('proposal-article.participant.plural')}`
-          }
-        </p>
-        <div className='social-links'>
-          <Social topic={topic}/>
-        </div> 
-      </div>
-    </div>
-  </div>
+    </Link>
   )
 }
